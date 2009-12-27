6ce810f94baa9ad246e849be2dff1fd9d8b131ff
riley <riley@6f4416e5-51e4-46ab-bb73-ab9a69715724>
2009-12-27 05:07:25 +0000

    initial check in

---
+++ Tester/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibUsbDotNet;
+using LibUsbDotNet.DeviceNotify;
+using LibUsbDotNet.Main;
+using LibUsbDotNet.Info;
+using System.Diagnostics;
+using usbSimInstrument;
+
+namespace Tester
+{
+    public partial class Form1 : Form
+    {
+        public static DeviceNotifier DeviceNotifier = new DeviceNotifier();
+        private Instrument MyInstrument;
+
+        const int VENDOR_ID = 0x16C0;
+        const int PRODUCT_ID = 0x05DC;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DeviceNotifier.OnDeviceNotify += OnDeviceNotifyEvent;
+            FindDevices();
+        }
+
+        private void FindDevices()
+        {
+            lbDevices.Items.Clear();
+
+            UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(VENDOR_ID, PRODUCT_ID);
+            UsbRegDeviceList MyUsbRegDeviceList = UsbGlobals.AllDevices.FindAll(MyUsbFinder);
+            foreach (UsbRegistry MyUsbRegistry in MyUsbRegDeviceList)
+            {
+                lbDevices.Items.Add(String.Format("{0} - {1}", MyUsbRegistry.Device.Info.SerialString.ToString(), MyUsbRegistry.Name));
+            }
+        }
+
+        private void OnDeviceNotifyEvent(object sender, DeviceNotifyEventArgs e)
+        {
+            if ((e.Device.IdVendor == VENDOR_ID) && (e.Device.IdProduct == PRODUCT_ID))
+            {
+
+                switch(e.EventType)
+                {
+                    case EventType.DeviceArrival:
+                        FindDevices();
+                        break;
+                    case EventType.DeviceRemoveComplete:
+                        FindDevices();
+                        break;
+                }
+            }
+        }
+
+        private void lbDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String[] pieces = lbDevices.SelectedItem.ToString().Split(' ');
+            MyInstrument = new Instrument(pieces[0]);
+            MyInstrument.Set1(trackBar1.Value);
+        }
+
+        private static void ShowLastUsbError()
+        {
+            Debug.WriteLine(String.Format("Error Number: {0}", UsbGlobals.LastErrorNumber));
+            Debug.WriteLine(UsbGlobals.LastErrorString);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+            if (MyInstrument != null)
+            {
+                MyInstrument.Set1(trackBar1.Value);
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (MyInstrument != null)
+            {
+                double val;
+                lbData.Items.Clear();
+                for (int i = 0; i < 8; i++)
+                {
+                    val = MyInstrument.ReadTable(i);
+                    lbData.Items.Add(val.ToString());
+                }
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            if ((MyInstrument != null) && (tbWriteData.Text.Length >= 15))
+            {
+                String[] values = System.Text.RegularExpressions.Regex.Split(tbWriteData.Text, "\r\n");
+                for (int i = 0; i < 8; i++)
+                {
+                    Debug.WriteLine(String.Format("Writing table value[{0}]: {1}", i, values[i]));
+                    MyInstrument.WriteTable(i, double.Parse(values[i]));
+                }
+            }
+        }
+    }
+}
---
+++ usbSimInstrument/Instrument.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
+
+namespace usbSimInstrument
+{
+    public class Instrument
+    {
+        private static UsbDevice usbInstrumentDevice;
+
+        internal const int VENDOR_ID = 0x16C0;
+        internal const int PRODUCT_ID = 0x05DC;
+
+        internal enum USBSIM_COMMANDS
+        {
+            ECHO = 0,
+            SET1 = 1,
+            SET2 = 2,
+            GET_TABLE = 3,
+            SET_TABLE = 4,
+            RESET = 9,
+        };
+
+        public Instrument(String serial)
+        {
+            UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(VENDOR_ID, PRODUCT_ID, serial);
+            UsbRegistry MyUsbRegistry = UsbGlobals.AllDevices.Find(MyUsbFinder);
+            if (MyUsbRegistry == null)
+            {
+                Debug.WriteLine(String.Format("Could not find device with serial {0}", serial));
+            }
+            else
+            {
+                if (!MyUsbRegistry.Open(out usbInstrumentDevice))
+                {
+                    Debug.WriteLine(string.Format("Could not open device with serial {0}", serial));
+                }
+            }
+        }
+
+        public bool Set1(int val)
+        {
+            if (usbInstrumentDevice.Open())
+            {
+                int lengthTransferred;
+                byte[] buf = new byte[1];
+
+                UsbSetupPacket cmd = UsbCmdSet1;
+                cmd.Value = (short)val;
+                bool bSuccess = usbInstrumentDevice.ControlTransfer(ref cmd, buf, buf.Length, out lengthTransferred);
+                return bSuccess && lengthTransferred == 1;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double ReadTable(int addr)
+        {
+            if (usbInstrumentDevice.Open())
+            {
+                int lengthTransferred;
+                byte[] buf = new byte[8];
+
+                UsbSetupPacket cmd = UsbCmdReadTable;
+                cmd.Value = (short)addr;
+                if (usbInstrumentDevice.ControlTransfer(ref cmd, buf, buf.Length, out lengthTransferred))
+                {
+                    return BitConverter.ToDouble(buf, 0);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool WriteTable(int addr, double val)
+        {
+            if (usbInstrumentDevice.Open())
+            {
+                int lengthTransferred;
+                byte[] buf = new byte[1];
+
+                UsbSetupPacket cmd = UsbCmdWriteTable;
+                byte[] addrBits = new byte[2];
+                addrBits[0] = (byte)addr;
+                bool allOK = false;
+                byte[] valBits = BitConverter.GetBytes(val);
+                for (byte i = 0; i < 8; i++)
+                {
+                    addrBits[1] = i;
+                    cmd.Value = (short)BitConverter.ToInt16(addrBits, 0);
+                    cmd.Index = (short)valBits[i];
+                    bool bSuccess = usbInstrumentDevice.ControlTransfer(ref cmd, buf, buf.Length, out lengthTransferred);
+                    allOK = bSuccess && lengthTransferred == 1;
+                }
+                return allOK;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal static readonly UsbSetupPacket UsbCmdSet1 =
+            new UsbSetupPacket(UsbRequestType.EndpointIn | UsbRequestType.RecipDevice | UsbRequestType.TypeVendor,
+                               (DeviceRequestType)USBSIM_COMMANDS.SET1,
+                               0,
+                               0,
+                               1);
+        internal static readonly UsbSetupPacket UsbCmdWriteTable =
+            new UsbSetupPacket(UsbRequestType.EndpointIn | UsbRequestType.RecipDevice | UsbRequestType.TypeVendor,
+                               (DeviceRequestType)USBSIM_COMMANDS.SET_TABLE,
+                               0,
+                               0,
+                               1);
+        internal static readonly UsbSetupPacket UsbCmdReadTable =
+            new UsbSetupPacket(UsbRequestType.EndpointIn | UsbRequestType.RecipDevice | UsbRequestType.TypeVendor,
+                               (DeviceRequestType)USBSIM_COMMANDS.GET_TABLE,
+                               0,
+                               0,
+                               1);
+    }
+}
